13adb0f51a83a72ee629926fdf46b4bb38c4141d
bgpaten <ahyarpattani@gmail.com>
2024-03-06 21:39:37 +0700

    Edit Pinjam DONE

--- app/Http/Controllers/PinjemController.php
+++ app/Http/Controllers/PinjemController.php
@@ -61,30 +61,54 @@ class PinjemController extends Controller
 
     public function show(string $id)
     {
-        //
+        
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
+    public function edit($id)
     {
-        //
+        $user = User::all();
+        $barang = Barang::all();
+        $data = Pinjam::findOrFail($id);
+        return view('pinjambarang.edit', compact( 'user','barang', 'data'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
-        //
+        $update = Pinjam::find($id);
+
+        $rules = [
+            'peminjam' => 'required',
+            'jumlah' => 'required',
+            'nama' => 'required',
+            'kode' => 'required',
+            'tgl_pinjam' => 'required',
+            'tgl_kembali' => 'required'
+        ];
+
+        $messages = [
+            'required' => ':attribute tidak boleh kosong',
+        ];
+
+        $this->validate($request, $rules, $messages);
+
+        $update->user_id = $request->peminjam;
+        $update->kode_barang = $request->kode;
+        $update->nama_barang = $request->nama;
+        $update->jumlah_pinjam = $request->jumlah;
+        $update->created_at = $request->tgl_pinjam;
+        $update->tgl_kembali = $request->tgl_kembali;
+        
+        $update->save();
+        Alert::success('Successpull', 'Data Berhasil di Update');
+        return redirect()->route('pinjem.index');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        //
+        $pinjam = Pinjam::findOrFail($id);
+        $pinjam->delete();
+        Alert::success('Successpull', 'Data Berhasil di Hapus');
+        // redirect
+        return redirect()->route('pinjem.index');
     }
 }
